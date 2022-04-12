41ef1873794ed7254591d88cafd02e87047d2892
Alexander <shursaw@gmail.com>
2022-04-12 14:26:51 +0300

    dff

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 "use strict";
-alert('rtrt');
+//alert('rtrt');
+const numberOfFilms = prompt('сколько фильмов', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('последний фильм', ''),
+      b = prompt('на сколько оцкниваете', ''),
+      c = prompt('последний фильм', ''),
+      d = prompt('на сколько оцкниваете', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
